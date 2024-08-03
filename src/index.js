4b43db69e7f0e9ad46ed5ec0b6197a43d64c5696
stenyukovs <stenyukovs@gmail.com>
2024-08-03 21:34:17 +0300

    fix:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-module.exports = function toReadable (number) {
-    if (number == 0)
-        return 'zero'
+function toReadable(number) {
+    if (number == 0) return 'zero'
     let numberString = String(number);
     let static = new Map([
         ["0", ""],
@@ -28,31 +27,26 @@ module.exports = function toReadable (number) {
     var hundreds = 0;
     var tens = 0;
     var result = "";
-    if (number <= 20) {
-        return static.get(String(number));
-    }
+    if (number <= 20) return static.get(String(number));
     if (numberString.length == 3) {
         result += static.get(numberString[0]);
         result += " hundred ";
     }
-    if (numberString.length > 1 ) {
-        var temp = numberString.length == 2 ? numberString[0] : numberString[1]
-        if (numberString[1] == '1') {
+    if (numberString.length > 1) {
+        var temp = numberString.length == 2 ? numberString[0] : numberString[1];
+        if (numberString[1] == "1") {
             result += static.get(numberString[1] + numberString[2]);
             return result;
-        } else if (temp == '2'){
-            result += 'twen';
-        } else if (temp == '3'){
-            result += 'thir';
-        } else if (temp == '5'){
-            result += 'fif';
-        }else if (temp == '8'){
-            result += 'eigh';
-        } else {
-            result += static.get(temp);
-        }
+        } else if (temp == "2") result += "twen";
+        else if (temp == "3") result += "thir";
+        else if (temp == "5") result += "fif";
+        else if (temp == "8") result += "eigh";
+        else result += static.get(temp);
+        if (numberString[1] != '0')
         result += "ty ";
     }
     result += static.get(numberString[numberString.length - 1]);
     return result;
 }
+
+console.log(toReadable(202))
